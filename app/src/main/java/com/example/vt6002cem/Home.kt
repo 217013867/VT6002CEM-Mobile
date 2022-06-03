9c42931832666ef6f2ca473759ddeedffef7f867
itsky123 <sky941129@gmail.com>
2022-06-03 18:07:36 +0800

    finished login & logout

---
+++ app/src/main/java/com/example/vt6002cem/Home.kt
@@ -0,0 +1,73 @@
+package com.example.vt6002cem
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.vt6002cem.databinding.ActivityHomeBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
+
+
+class Home : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var mAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        mAuth = FirebaseAuth.getInstance();
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_home)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                true
+            }
+            R.id.action_logout -> {
+                logout()
+                Toast.makeText(
+                    this,
+                    "Logout successfully",
+                    Toast.LENGTH_SHORT
+                ).show()
+                startActivity(Intent(this, MainActivity::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun logout() {
+        mAuth.signOut()
+    }
+}
--- app/src/main/java/com/example/vt6002cem/Login.kt
+++ app/src/main/java/com/example/vt6002cem/Login.kt
@@ -1,9 +1,12 @@
 package com.example.vt6002cem
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.vt6002cem.databinding.FragmentLoginBinding
@@ -36,9 +39,36 @@ class Login : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
+        binding.loginBtn.setOnClickListener {
+
             // connect to firebase
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            val email: String = binding.editTextTextEmailAddress.text.toString()
+            val password: String = binding.editTextTextPassword.text.toString()
+
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(requireActivity(), "Email cannot be empty", Toast.LENGTH_SHORT)
+                    .show()
+            } else if (TextUtils.isEmpty(password)) {
+                Toast.makeText(requireActivity(), "Password cannot be empty", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            requireActivity(),
+                            "User logged in successfully",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        startActivity(Intent(requireActivity(), Home::class.java))
+                    } else {
+                        Toast.makeText(
+                            requireActivity(),
+                            "Log in Error",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
         }
 
         binding.tvRegisterHere.setOnClickListener { view ->
