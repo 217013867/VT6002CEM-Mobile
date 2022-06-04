81289e80cf28e9452a051f6e4248aa8f2cfe92c1
itsky123 <sky941129@gmail.com>
2022-06-04 11:29:48 +0800

    Add Detection model

---
+++ app/src/main/java/com/example/vt6002cem/Detection.kt
@@ -0,0 +1,9 @@
+package com.example.vt6002cem
+
+import android.widget.Button
+
+class Detection {
+    var select: Button = findViewById(R.id.Button)
+
+    select.setOnClickListener(View.onClickListener)
+}
--- app/src/main/java/com/example/vt6002cem/Register.kt
+++ app/src/main/java/com/example/vt6002cem/Register.kt
@@ -11,6 +11,9 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.vt6002cem.databinding.FragmentRegisterBinding
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import okhttp3.*
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody.Companion.toRequestBody
@@ -63,79 +66,107 @@ class Register : Fragment() {
     }
 
     private fun createUser() {
-        val unixTime: String = Instant.now().getEpochSecond().toString()
+        CoroutineScope(Dispatchers.IO).launch {
+            val client = OkHttpClient()
+            val mediaType = "application/json".toMediaType()
+            val bodyReq = "{\n" +
+                    "      \"dataSource\": \"Cluster1\",\n" +
+                    "      \"database\": \"FishApp\",\n" +
+                    "      \"collection\": \"users\",\n"
 
-        println(binding.editTextTextEmailAddress.text)
-        val username = binding.editTextTextUsername.text.toString()
-        val email = binding.editTextTextEmailAddress.text.toString()
-        val password = binding.editTextTextPassword.text.toString()
+            val unixTime: String = Instant.now().getEpochSecond().toString()
 
-        if (TextUtils.isEmpty(username)) {
-            Toast.makeText(requireActivity(), "Username cannot be empty", Toast.LENGTH_SHORT).show()
-        } else if (TextUtils.isEmpty(email)) {
-            Toast.makeText(requireActivity(), "Email cannot be empty", Toast.LENGTH_SHORT).show()
-        } else if (TextUtils.isEmpty(password)) {
-            Toast.makeText(requireActivity(), "Password cannot be empty", Toast.LENGTH_SHORT).show()
-        } else {
-            // insert record into mongodb
-            val jsonObject = JSONObject()
-            try {
-                jsonObject.put("dataSource", "Cluster1")
-                jsonObject.put("database", "FishApp")
-                jsonObject.put("collection", "users")
-
-                val documentObject = JSONObject()
-                documentObject.put("username", username)
-                val dateObject = JSONObject()
-                val numberLongObject = JSONObject()
-
-                numberLongObject.put("\$numberLong", unixTime.toString())
-                dateObject.put("\$date", numberLongObject)
-                documentObject.put("createTime", dateObject)
-                jsonObject.put("document", documentObject)
-            } catch (e: JSONException) {
-                e.printStackTrace()
-            }
-
-            Log.d("RequestBody", jsonObject.toString())
-
-            val requestBody = jsonObject.toString().toRequestBody(JSON)
-
-            val request: Request = Request.Builder()
+            val request = Request.Builder()
                 .url("https://data.mongodb-api.com/app/data-grmrc/endpoint/data/v1/action/insertOne")
-                .method("POST", requestBody)
-                .addHeader(
+                .header("Content-Type", "application/json")
+                .header(
                     "api-key",
                     "VgP7ObwftcYymjetp5C5LVyqBgbWDSrETQ4njGISc7WyLi4tqG2WiOB7QLTaWpiD"
                 )
-                .addHeader("Content-Type", "application/json")
+                .post(bodyReq.toRequestBody(mediaType))
                 .build()
 
-//
+            println(binding.editTextTextEmailAddress.text)
+            val username = binding.editTextTextUsername.text.toString()
+            val email = binding.editTextTextEmailAddress.text.toString()
+            val password = binding.editTextTextPassword.text.toString()
 
-            val buffer = Buffer()
-            request.body?.writeTo(buffer)
-            Log.d(">>>>", buffer.readUtf8().toString())
+            if (TextUtils.isEmpty(username)) {
+                Toast.makeText(requireActivity(), "Username cannot be empty", Toast.LENGTH_SHORT)
+                    .show()
+            } else if (TextUtils.isEmpty(email)) {
+                Toast.makeText(requireActivity(), "Email cannot be empty", Toast.LENGTH_SHORT)
+                    .show()
+            } else if (TextUtils.isEmpty(password)) {
+                Toast.makeText(requireActivity(), "Password cannot be empty", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                // insert record into mongodb
+                val jsonObject = JSONObject()
+                try {
+                    jsonObject.put("dataSource", "Cluster1")
+                    jsonObject.put("database", "FishApp")
+                    jsonObject.put("collection", "users")
 
-            client.newCall(request).enqueue(object : Callback {
-                override fun onFailure(call: Call, e: IOException) {
+                    val documentObject = JSONObject()
+                    documentObject.put("username", username)
+                    val dateObject = JSONObject()
+                    val numberLongObject = JSONObject()
+
+                    numberLongObject.put("\$numberLong", unixTime.toString())
+                    dateObject.put("\$date", numberLongObject)
+                    documentObject.put("createTime", dateObject)
+                    jsonObject.put("document", documentObject)
+                } catch (e: JSONException) {
                     e.printStackTrace()
                 }
 
-                override fun onResponse(call: Call, response: Response) {
-                    response.use {
-                        if (!response.isSuccessful) throw IOException("Unexpected code $response")
+//            Log.d("RequestBody", jsonObject.toString())
+//
+//            val requestBody = jsonObject.toString().toRequestBody(JSON)
+//
+//            val request = Request.Builder()
+//                .url("https://data.mongodb-api.com/app/data-grmrc/endpoint/data/v1/action/insertOne")
+//                .header("Content-Type", "application/json")
+//                .header("api-key", "VgP7ObwftcYymjetp5C5LVyqBgbWDSrETQ4njGISc7WyLi4tqG2WiOB7QLTaWpiD")
+//                .post(bodyReq.toRequestBody(MediaType)
+//                .build()
 
-                        for ((name, value) in response.headers) {
-                            Log.d(name, value)
-                        }
+//            val request: Request = Request.Builder()
+//                .url("https://data.mongodb-api.com/app/data-grmrc/endpoint/data/v1/action/insertOne")
+//                .method("POST", requestBody)
+//                .addHeader(
+//                    "api-key",
+//                    "VgP7ObwftcYymjetp5C5LVyqBgbWDSrETQ4njGISc7WyLi4tqG2WiOB7QLTaWpiD"
+//                )
+//                .addHeader("Content-Type", "application/json")
+//                .build()
 
-                        Log.d(response.body!!.string(), "<<<")
+//
+
+                val buffer = Buffer()
+                request.body?.writeTo(buffer)
+                Log.d(">>>>", buffer.readUtf8().toString())
+
+                client.newCall(request).enqueue(object : Callback {
+                    override fun onFailure(call: Call, e: IOException) {
+                        e.printStackTrace()
                     }
-                }
-            })
 
-            // 執行Call連線到網址
+                    override fun onResponse(call: Call, response: Response) {
+                        response.use {
+                            if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+                            for ((name, value) in response.headers) {
+                                Log.d(name, value)
+                            }
+
+                            Log.d(response.body!!.string(), "<<<")
+                        }
+                    }
+                })
+
+                // 執行Call連線到網址
 //            call.enqueue(object : Callback {
 //                @Throws(IOException::class)
 //                override fun onResponse(call: Call?, response: Response) {
@@ -150,23 +181,25 @@ class Register : Fragment() {
 //                }
 //            })
 
-            // create user in firebase
-            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    Toast.makeText(
-                        requireActivity(),
-                        "User registered successfully",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    findNavController().navigate(R.id.action_RegisterFragment_to_LoginFragment)
+                // create user in firebase
+                mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(
+                                requireActivity(),
+                                "User registered successfully",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            findNavController().navigate(R.id.action_RegisterFragment_to_LoginFragment)
 
-                } else {
-                    Toast.makeText(
-                        requireActivity(),
-                        "Registration Error",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
+                        } else {
+                            Toast.makeText(
+                                requireActivity(),
+                                "Registration Error",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
             }
         }
     }
