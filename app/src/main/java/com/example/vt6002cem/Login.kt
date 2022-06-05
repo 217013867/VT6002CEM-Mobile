a5a870a98dcfa6e3741a0bce5a6ab9420704d899
itsky123 <sky941129@gmail.com>
2022-06-05 17:06:34 +0800

    add bio login

--- app/src/main/java/com/example/vt6002cem/Login.kt
+++ app/src/main/java/com/example/vt6002cem/Login.kt
@@ -7,10 +7,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
+import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.vt6002cem.databinding.FragmentLoginBinding
 import com.google.firebase.auth.FirebaseAuth
+import java.util.concurrent.Executor
 
 
 /**`
@@ -24,6 +30,82 @@ class Login : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
     private lateinit var mAuth: FirebaseAuth
+
+    private lateinit var executor: Executor
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
+
+
+    override fun onResume() {
+        super.onResume()
+        val biometricStatusTextView = binding.biometricStatus
+        val biometricManager = BiometricManager.from(requireActivity())
+        when (biometricManager.canAuthenticate(BIOMETRIC_STRONG or DEVICE_CREDENTIAL)) {
+            BiometricManager.BIOMETRIC_SUCCESS ->
+                biometricStatusTextView.text = "App can authenticate using biometrics."
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE ->
+                biometricStatusTextView.text = "No biometric features available on this device."
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE ->
+                biometricStatusTextView.text = "Biometric features are currently unavailable."
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED ->
+                // Prompts the user to create credentials that your app accepts.
+                biometricStatusTextView.text = "Biometric features are not enrolled."
+        }
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        executor = ContextCompat.getMainExecutor(requireContext())
+
+        biometricPrompt = BiometricPrompt(requireActivity(), executor,
+            object : BiometricPrompt.AuthenticationCallback() {
+
+                override fun onAuthenticationError(
+                    errorCode: Int,
+                    errString: CharSequence
+                ) {
+                    super.onAuthenticationError(errorCode, errString)
+                    Toast.makeText(
+                        requireContext(),
+                        "Authentication error: $errString", Toast.LENGTH_SHORT
+                    )
+                        .show()
+                }
+
+                override fun onAuthenticationSucceeded(
+                    result: BiometricPrompt.AuthenticationResult
+                ) {
+                    super.onAuthenticationSucceeded(result)
+                    startActivity(Intent(requireActivity(), Home::class.java))
+                    Toast.makeText(
+                        requireContext(),
+                        "Authentication succeeded!", Toast.LENGTH_SHORT
+                    )
+                        .show()
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    Toast.makeText(
+                        requireContext(), "Authentication failed",
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+                }
+
+            })
+
+        promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric login for my app")
+            .setSubtitle("Log in using your biometric credential")
+            .setNegativeButtonText("Use account password")
+            .build()
+
+        val biometricLoginButton = binding.biometricLogin
+        biometricLoginButton.setOnClickListener {
+            biometricPrompt.authenticate(promptInfo)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
