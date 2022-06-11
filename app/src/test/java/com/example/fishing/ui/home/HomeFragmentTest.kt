2bd9d4dbdcf9c4e634e22c02ab4cbea0981fe84e
itsky123 <sky941129@gmail.com>
2022-06-11 21:39:11 +0800

    Add Test

--- app/src/test/java/com/example/fishing/ui/home/HomeFragmentTest.kt
+++ app/src/test/java/com/example/fishing/ui/home/HomeFragmentTest.kt
@@ -1,34 +1,25 @@
 package com.example.fishing.ui.home
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
+
 class HomeFragmentTest {
-
     @Test
-    fun checkAndGetPermissions() {
+    fun getMaxTest() {
+        assertEquals(1000, getMax(FloatArray(1001) { 10 * (it + 1F) }))
     }
+}
 
-    @Test
-    fun onRequestPermissionsResult() {
-    }
+fun getMax(arr: FloatArray): Int {
+    var ind = 0
+    var min = 0.0f
 
-    @Test
-    fun onCreateView() {
+    for (i in 0..1000) {
+        if (arr[i] > min) {
+            ind = i
+            min = arr[i]
+        }
     }
-
-    @Test
-    fun onActivityCreated() {
-    }
-
-    @Test
-    fun onActivityResult() {
-    }
-
-    @Test
-    fun onDestroyView() {
-    }
-
-    @Test
-    fun getMax() {
-    }
+    return ind
 }
